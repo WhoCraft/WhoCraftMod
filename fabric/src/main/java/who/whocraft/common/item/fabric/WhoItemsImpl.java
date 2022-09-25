80fb42ca905fe7dfdd7a389de8675b4fea4800f9
Craig <sandedshoes@gmail.com>
2022-09-25 10:23:06 +0100

    Creative tabs can now be created

---
+++ fabric/src/main/java/who/whocraft/common/item/fabric/WhoItemsImpl.java
@@ -0,0 +1,18 @@
+package who.whocraft.common.item.fabric;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import who.whocraft.Whocraft;
+
+public class WhoItemsImpl {
+
+    public static final CreativeModeTab TAB = FabricItemGroupBuilder.build(new ResourceLocation(Whocraft.MODID, Whocraft.MODID), () -> new ItemStack(Blocks.REDSTONE_WIRE));
+
+    public static CreativeModeTab getCreativeTab() {
+        return TAB;
+    }
+
+}
